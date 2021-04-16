849ad917865ad25e18a0dd0e5d921948c513fcb5
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2021-04-16 09:48:42 -0300

    Coding imperative routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
 
 @NgModule({
   declarations: [
@@ -16,14 +19,15 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
     CursosComponent,
     HomeComponent,
     LoginComponent,
-    CursoDetalheComponent
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     routing
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+
+import { CursosService } from './../cursos/cursos.service';
+
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,10 +12,14 @@ import { Subscription } from 'rxjs';
 })
 export class CursoDetalheComponent implements OnInit {
   
-  id: string = '';
+  id: number = 0;
   subscription: Subscription = new Subscription;
+  curso: any;
 
-  constructor(private route: ActivatedRoute) {    
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private cursosService: CursosService) {    
     //this.id = this.route.snapshot.params['id'];
     //console.log(this.route);
   }
@@ -22,6 +29,11 @@ export class CursoDetalheComponent implements OnInit {
     this.subscription = this.route.params.subscribe(
       (params: any) => {
         this.id = params['id'];
+        this.curso = this.cursosService.getCurso(this.id);
+
+        if (this.curso == null) {
+          this.router.navigate(['/naoEncontrado']);
+        }
       }
     );
   }
